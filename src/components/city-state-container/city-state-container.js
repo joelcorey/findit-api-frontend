43204947e169fedbf34feb30dca5913014fec60a
Joel Corey <joelcorey@fastmail.com>
2020-05-19 15:08:14 -0700

    nowork fetchy fetch

--- src/components/city-state-container/city-state-container.js
+++ src/components/city-state-container/city-state-container.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './city-state-container.css';
 
 import ResultInfo from '../result-info/result-info';
 
-const CityStateContainer = () => {
+export default function  CityStateContainer() {
+
+    const [ state, setState ] = useState({  });
+
+    async function fetchData() {
+        // send HTTP request
+        const response = await fetch('http://localhost:8000/cities',
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                state: "Oregon"
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+        setState(data);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     return (
         <div className="city-state-container">
@@ -16,4 +36,3 @@ const CityStateContainer = () => {
     );
 }
 
-export default CityStateContainer;
