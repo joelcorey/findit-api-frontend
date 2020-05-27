e4af4d438460d8b06c15324b320ac4c01377b7c9
Joel Corey <joelcorey@fastmail.com>
2020-05-27 10:55:35 -0700

    format css

--- src/components/result-info/result-info.js
+++ src/components/result-info/result-info.js
@@ -6,6 +6,7 @@ const ResultInfo = (props) => {
     return (
         <div className="result-info bg-background-dark" key={props.index}>
             <div className="result-info-date">{props.month}-{props.day}-{props.year}</div>
+            <div className="result-info-time"> {props.timeString}</div>
             <div className="result-info-item result-info-link"><a href={props.url} target="_blank">{props.title}</a></div>
         </div>
     );
