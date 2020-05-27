9426a7ac09f0f5b25f67f3ac238125b6ce8ff9af
Joel Corey <joelcorey@fastmail.com>
2020-05-27 08:10:14 -0700

    procedural scrape

--- src/components/city-container/city-container.js
+++ src/components/city-container/city-container.js
@@ -5,39 +5,56 @@ import ResultInfo from '../result-info/result-info';
 
 const CityTerritoryContainer = (props) => {
 
-    //const [ jobs, setJobs ] = useState([]);
-    const [ loading, setLoading ] = useState(true);
-    // 
+    const [ loading, setLoading ] = useState(true)
     const [ jobs, setJobs ] = useState()
-    const [ isCurrentState, setIsCurrentState ] = useState(false);
-
-    let getCityJobs;
+    const [ isCurrentState, setIsCurrentState ] = useState();
     
+    let getCityJobs;
+
+    if(props.currentState === props.territor) {
         getCityJobs = props.useFetch('http://localhost:8000/jobs',
         {
         method: 'POST',
         body: JSON.stringify({ url: props.url }),
         headers: {"Content-Type": "application/json"}
         });
+        
     }
 
     useEffect(() => {
-        if (props.currentState === props.territory) {
-            setIsCurrentState(true)
-        }
-    } )
+
+    })
 
     useEffect(() => {
         setJobs(getCityJobs.response);
-    }, [isCurrentState] )
+        setLoading(false);
+    }, [getCityJobs] )
 
-    if(!loading) {
+    if(!loading && jobs !== null) {
         return (
-            <div >
-                Done loading
-            </div>
+            <>
+                <div>{props.territory}</div>
+                <div>{props.city}</div>
+                <div>{props.url}</div>
+                <div>
+                    {jobs.map((job, i) => {
+                        return <ResultInfo 
+                            key={i}
+                            title={job.resultTitleText}
+                            url={job.resultTitleHref}
+                            dateTitle={job.date.dateTitle}
+                            dateString={job.date.dateString}
+                            timeString={job.date.timeString}
+                            year={job.date.year}
+                            month={job.date.month}
+                            day={job.date.day}
+                        />
+                    })}
+                </div>
+            </>
         )
     }
+
     return (
         <div>
             loading
@@ -45,6 +62,5 @@ const CityTerritoryContainer = (props) => {
     )
     
 }
-
 
 export default CityTerritoryContainer;
