068e7214b5a5b4475e6983b5f37d4ee62fb0dc76
Joel Corey <joelcorey@fastmail.com>
2020-06-08 20:23:29 -0700

    css fixes

--- src/util/filters.js
+++ src/util/filters.js
@@ -1,5 +1,6 @@
 function filter() {
     return [
+        // Non-specific / generic here
         'Webcam models required', // example of long lived filter .. more generic
         'Bottom guy for top', // woah .. this is a job board, highly inappropriate!
         'Online Advertise',
@@ -10,18 +11,32 @@ function filter() {
         'Telemarketing',
         'Have a Facebook profile',
         'Receptionist',
+        'Travel Agent',
         '5 Minute',
         'Survey',
         'Book travel packages', // No, don't want to do that!
+        'looking for friend',
+        'Customer Service',
+        'TRAVEL AGENTS',
+        'internet marketer',
+        'Internet Sales & Marketing',
+        'sales agents',
+        'marketing sales',
+        'marketing job',
+        'Media Marketing',
+        'online survey',
+        'Bilingual',
+        'Looking for Social Media help in',
+        'Social Media Assistant',
+        'Piece of Mail',
+        'edit videos',
+
+        
+        // Specific here
         'IT/Cybersecurity Training Grants with Job Placement Assistance.', // Semi-applicable but a bit spammy as this job posting had 30 plus postings .. all of which show up in the scraped results
         'Work Safely from Home, Online, Paid Weekly',
         'I AM IN A HOTEL AND I WANT TO USE CHROMECAST - I HAVE TRAVEL ROUTER', // An example of highly annoying more short term filter. Who the hell thinks it's okay to use all caps like this.. savages.
-        'Customer Service Representatives Needed',
-        'TRAVEL AGENTS WANTED',
-        'internet marketer',
         'Hiring aomeone to make asmr youtube videos', // Yikes! No thank you.
-        'sales agents',
-        'marketing sales',
         'In Need of Social Media Help for Podcast',
         'Two individuals needed as partners in a web hosting business.',
         'Help Get Financing for a Letter of Credit that I Receive from Overseas', // Hello scammer!'
@@ -30,6 +45,41 @@ function filter() {
         'Promote Texbot Ai Earn 100.00 A Day From Home',
         'Online research study for young adult smokers',
         'Looking for low-voltage expert',
+        'WANTED HANDYMAN / CONTRACTOR',
+        'Have you expunged or sealed a criminal case? We’ll pay you $50 to talk',
+        '10 Easy GIGS Anyone Can Do To Make Money!',
+        'Phone PC/MAC Repair Technician - Free Training',
+        'This is a business, not a job. You are Guarantee to make money your fi',
+        'SAHMS TO WORK AT HOME-NEW CAREER AS TRAVEL AGENT',
+        'Looking for Instagram Aficionado',
+        'Apple device training',
+        'Travel Coordinator Remote',
+        'Work Part time from Home...',
+        'Earn an Extra $1K-$2K+ per month PT / $100K+ Annually FT (From Home!)',
+        'Social Media Marketing Expert',
+        'FOREX & STOCKS TRADER... LOOKING 4 PARTNERSHIP....',
+        'EARN $$1000 IN DIGITAL CURRENCY',
+        'Work from home marketing job - No college',
+        'Looking for people to test my new Weight Loss Competition Mobile App!',
+        '▬► Social Media Manager (Part Time)',
+        'Service Field Technicians (North Milwaukee Area)',
+        'Need Help with Physics Course',
+        'Seeking Salesperson to close b2b clients for Video Production Company',
+        'On-Air talent (Savvy woman)to host financial wellness YT/Podcast',
+        '**WORK FROM HOME TELEPHONE MYSTERY SHOPPERS NEEDED**',
+        '*Remote Work* Product Testing Legal Online Casinos $100-1,000',
+        '***Make an Extra $500/Week Mailing Our Postcards***',
+        'Adults Ages 60+ Make Money Sharing Your Expertise (Online)',
+        'Curtsy is hiring a Customer Success Associate (Work From Home)',
+        'NEED TELEPHONE LINES HOOKED UP ASAP TODAY IN WAKEFIELD',
+        'Cakewalk by Bandlab',
+        'Paid job looking for anybody with a tax number',
+        'CASH for your opinion!',
+        'Recovering text messages',
+        'Weight Loss Competition Mobile App',
+        'GREAT WORK FROM HOME OPPORTUNITY FOR MARKETERS!!!',
+        '"GET PAID TO TEXT"',
+
 
     ];
 }
